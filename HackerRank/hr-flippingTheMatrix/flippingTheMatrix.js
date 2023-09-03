6096cdcbf3a04ad786ea42c293fbd0a2308ac454
Yoon Hwan Cho <106636383+YoonHCho@users.noreply.github.com>
2023-09-02 22:36:52 -0700

    revised

--- HackerRank/hr-flippingTheMatrix/flippingTheMatrix.js
+++ HackerRank/hr-flippingTheMatrix/flippingTheMatrix.js
@@ -1,11 +1,12 @@
 function flippingMatrix(matrix) {
   // mirrored value (quadrant - please see md file) only need to run the upper right.
   const quad = matrix.length / 2;
-  let maxVal = 0;
+  let maxVal;
   let sum = 0;
 
   for (let row = 0; row < quad; row++) {
     for (let col = 0; col < quad; col++) {
+      maxVal = Number.MIN_VALUE;
       maxVal = Math.max(matrix[row][col], maxVal);
       maxVal = Math.max(matrix[row][matrix.length - col - 1], maxVal);
       maxVal = Math.max(matrix[matrix.length - row - 1][col], maxVal);
