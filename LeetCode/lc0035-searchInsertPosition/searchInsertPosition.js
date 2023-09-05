b5a1694e6729b50b3f419181a4c20daeeb163c60
Yoon Hwan Cho <106636383+YoonHCho@users.noreply.github.com>
2023-09-04 22:16:45 -0700

    completed with O(log n)

--- LeetCode/lc0035-searchInsertPosition/searchInsertPosition.js
+++ LeetCode/lc0035-searchInsertPosition/searchInsertPosition.js
@@ -1,13 +1,27 @@
 var searchInsert = function (nums, target) {
-  // O(n)
-  const index = nums.indexOf(target);
-  if (index >= 0) return index;
-  for (let i = 0; i < nums.length; i++) {
-    if (nums[i] > target) {
-      return i;
+  // O (log n)
+  let low = 0,
+    high = nums.length;
+  while (low < high) {
+    let mid = Math.floor((low + high) / 2);
+    if (target === nums[mid]) return mid;
+    if (target > nums[mid]) {
+      low = mid + 1;
+    } else if (target < nums[mid]) {
+      high = mid;
     }
   }
-  return nums.length;
+  return low;
+
+  // O(n)
+  // const index = nums.indexOf(target);
+  // if (index >= 0) return index;
+  // for (let i = 0; i < nums.length; i++) {
+  //   if (nums[i] > target) {
+  //     return i;
+  //   }
+  // }
+  // return nums.length;
 };
 
 console.log(searchInsert([1, 3, 5, 6], 5)); // 2
