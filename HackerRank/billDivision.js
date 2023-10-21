03b33f5d3c2fcca82decc6d7602f55e2f4561ce6
Yoon Hwan Cho <106636383+YoonHCho@users.noreply.github.com>
2023-10-20 23:58:06 -0700

    completed hr algo

---
+++ HackerRank/billDivision.js
@@ -0,0 +1,23 @@
+// Link: https://www.hackerrank.com/challenges/bon-appetit/problem
+
+function bonAppetit(bill, k, b) {
+  let billToCalclate = 0;
+
+  for (let i = 0; i < bill.length; i++) {
+    if (i !== k) {
+      billToCalclate += bill[i];
+    }
+  }
+
+  const annaPortion = billToCalclate / 2;
+
+  if (annaPortion === b) {
+    return console.log("Bon Appetit");
+  }
+
+  return console.log(b - annaPortion);
+}
+
+bonAppetit([3, 10, 2, 9], 1, 12); // 5
+bonAppetit([3, 10, 2, 9], 1, 7); // Bon Appetit
+bonAppetit([2, 4, 6], 2, 3); // Bon Appetit
